d9057c9b877f91b4dda0c236142dd1a73992a9e8
Gabriel Duran Castillo <gaducamac@gmail.com>
2018-12-24 19:47:56 -0600

    start working with Jade engine

--- express_/index.js
+++ express_/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var path = require('path');
+var indexRouter=require('./routes/index');
 var app = express();
 
 /*Primer forma para poder usar archivos css*/
@@ -7,15 +9,19 @@ var app = express();
 /*Segunda forma para poder usar archivos css*/
 app.use(express.static(__dirname + '/public'));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use('/', indexRouter);
 
 app.get('/inicio', function(req, res){
 	res.send("<h1>Welcome to express</h1>");
 });
-
+/*
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/plantilla.html');
 });
-
-app.listen(3000, function(){
+*/
+app.listen(5000, function(){
 	console.log("----server on----");
 });
